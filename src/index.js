98cb3d917641cad3657b7c83ba69c6a6f39c7e80
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-06-22 17:28:46 -0500

    deployment v2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const io = socketio(server);
 
 
 const port = 3000;
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, './public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
